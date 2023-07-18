b208d658760bc55d3cb65425339ff6ec5c32d63e
NeranjanMe <newneranjan@gmail.com>
2023-07-18 13:03:02 +0530

    Create Category Pages and Backend

---
+++ author/category.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["username"])){
+    header("Location: ../login.php");
+    exit;
+}
+
+require_once '../database/db_connect.php';
+$db = connect_db();
+
+$stmt = $db->prepare("SELECT * FROM categories ORDER BY id DESC");
+$stmt->execute();
+$result = $stmt->get_result();
+$categories = $result->fetch_all(MYSQLI_ASSOC);
+
+include '../include/author_header.php';
+?>
+
+
+<?php include '../include/author_slidenav_head.php'; ?>
+
+                    <h1 class="mt-4">Category</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Category</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <!-- Add new category form -->
+                                <form action="../process/process_category.php" method="post">
+                                    <div class="form-group">
+                                        <label for="name">New Category Name</label>
+                                        <input type="text" class="form-control" id="name" name="name" required>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary mt-4">Add New Category</button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <!-- Categories table -->
+                            <div class="card mb-4 mt-5">
+                                <div class="card-header">
+                                    <i class="fas fa-table me-1"></i>
+                                    Categories Table
+                                </div>
+                                <div class="card-body">
+                                    <table id="datatablesSimple" class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <?php foreach ($categories as $category): ?>
+                                                <tr>
+                                                    <td><?php echo $category['id']; ?></td>
+                                                    <td><?php echo $category['name']; ?></td>
+                                                    <td>
+                                                        <a href="edit_category.php?id=<?php echo $category['id']; ?>" class="btn btn-primary">Edit</a>
+                                                        <a href="process_category.php?action=delete&id=<?php echo $category['id']; ?>" class="btn btn-danger">Delete</a>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+<?php include '../include/author_slidenav_down.php'; ?>
+
+<script>
+$(document).ready(function() {
+    $('#datatablesSimple').DataTable();
+});
+</script>
+
+
+<?php include '../include/author_footer.php'; ?>
+
+
---
+++ process/process_category.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../database/db_connect.php';
+session_start();
+
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["username"])){
+    header("Location: ../login.php");
+    exit;
+}
+
+$db = connect_db();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Add new category
+    $name = $_POST['name'];
+
+    if (empty($name)) {
+        die("Category name is required");
+    }
+
+    $stmt = $db->prepare("INSERT INTO categories (name) VALUES (?)");
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        header("Location: ../author/category.php"); // Redirect to the category page
+    } else {
+        die("Error adding category");
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['action'] === 'delete') {
+    // Delete a category
+    $id = $_GET['id'];
+
+    if (empty($id)) {
+        die("Category ID is required");
+    }
+
+    $stmt = $db->prepare("DELETE FROM categories WHERE id = ?");
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        header("Location: ../author/category.php"); // Redirect to the category page
+    } else {
+        die("Error deleting category");
+    }
+} else {
+    die("Invalid request");
+}
