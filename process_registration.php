ad0d1d47c1f828f201ee6aadb01d5a287373e477
NeranjanMe <newneranjan@gmail.com>
2023-07-18 11:12:25 +0530

    Create process_registration.php

---
+++ process_registration.php
@@ -0,0 +1,42 @@
+<?php
+// Database connection
+require_once 'db_connect.php';
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Input validation
+if (empty($username) || empty($password) || empty($email)) {
+    die("Username, email, and password are required");
+}
+
+// Prepared statement to prevent SQL Injection
+$stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param('s', $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Check if username already exists
+if ($result->num_rows > 0) {
+    die("Username already exists");
+} else {
+    // Hash the password and insert the new user into the database
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param('sss', $username, $email, $hashed_password);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        // Success! The user was registered. You might want to start a session here or send a confirmation email.
+        header("Location: login.php"); // Redirect to the login page or to a "Registration successful" page
+    } else {
+        // Error! The registration failed. Handle this error appropriately for your application.
+        die("Registration failed");
+    }
+}
+?>
