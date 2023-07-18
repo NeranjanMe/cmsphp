fe3f8e0c778d09112e93f67a5b1bdf49415188df
NeranjanMe <newneranjan@gmail.com>
2023-07-18 13:03:21 +0530

    Create author_slidenav_head.php

---
+++ include/author_slidenav_head.php
@@ -0,0 +1,26 @@
+<div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Addons</div>
+                            <a class="nav-link" href="post.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Post
+                            </a>
+                            <a class="nav-link" href="category.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Category
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                <div class="container-fluid px-4">
