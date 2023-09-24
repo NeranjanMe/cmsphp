947da4a899df38b0786758ca0ea14ba70c3aee92
NeranjanMe <newneranjan@gmail.com>
2023-09-25 00:01:37 +0530

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 function connect_db() {
-    $db = new mysqli('localhost', 'root', '', 'cmsdb');
+    $db = new mysqli('localhost', 'root', '-asDOomHsuBoN27B', 'upto_cmsdb');
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
     }
