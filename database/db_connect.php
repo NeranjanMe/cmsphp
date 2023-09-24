e434154ee36669ca7bd716b063ef2f4b72751474
NeranjanMe <newneranjan@gmail.com>
2023-09-25 00:31:42 +0530

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,9 +1,17 @@
 <?php
-function connect_db() {
-    $db = new mysqli('localhost', 'upto_root', '-asDOomHsuBoN27B', 'upto_cmsdb');
-    if ($db->connect_error) {
-        die("Connection failed: " . $db->connect_error);
-    }
-    return $db;
+$servername = "localhost";
+$database = "upto_cmsdb";
+$username = "upto_root";
+$password = "-asDOomHsuBoN27B";
+ 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error() . " - " . mysqli_connect_errno());
 }
+echo "Connected successfully";
+mysqli_close($conn);
+
 ?>
